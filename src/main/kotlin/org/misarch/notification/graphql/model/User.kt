3ee0e3cbe575df1620f0e10cc7124f2cb35b3184
nk-coding <niklask.coding@gmail.com>
2024-01-24 23:59:37 +0100

    add missing key decorator

--- src/main/kotlin/org/misarch/notification/graphql/model/User.kt
+++ src/main/kotlin/org/misarch/notification/graphql/model/User.kt
@@ -2,6 +2,8 @@ package org.misarch.notification.graphql.model
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
+import com.expediagroup.graphql.generator.federation.directives.FieldSet
+import com.expediagroup.graphql.generator.federation.directives.KeyDirective
 import org.misarch.notification.graphql.model.connection.NotificationConnection
 import org.misarch.notification.graphql.model.connection.NotificationOrder
 import org.misarch.notification.persistence.model.NotificationEntity
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import java.util.*
 
 @GraphQLDescription("A user.")
+@KeyDirective(fields = FieldSet("id"))
 class User(
     id: UUID
 ) : Node(id) {
